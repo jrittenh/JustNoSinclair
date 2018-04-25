33e8cf8017b8caaed051f47803d4e5a3b5f53df4
Justin Rittenhouse <jrittenh@nd.edu>
2018-04-24 22:11:21 -0400

    Reformatting post string, and rewording final sentence

--- justnosinclair.py
+++ justnosinclair.py
@@ -42,8 +42,16 @@ try:
                 and submission_url in domains \
                 and submission_timely:
                     print("SINCLAIR {submission.title} {submission.url}")
-
-                    submission.reply("The domain this post links to is owned or operated by [Sinclair Broadcast Group](https://en.wikipedia.org/wiki/List_of_stations_owned_or_operated_by_Sinclair_Broadcast_Group). PBS NewsHour [has reported on Sinclair's \"partisan tilt on trusted local news\"](https://youtu.be/zNhUk5v3ohE).  John Oliver has [featured a segment on his show](https://youtu.be/GvtNyOzGogc), Last Week Tonight, on Sinclair. Most recently, Sinclair has had an instance of numerous news reports using the exact same script for a Sinclair-provided segment [cut together and published on YouTube](https://youtu.be/hWLjYJ4BzvI).\n\nI am a bot. For any issues or to request to have your subreddit removed from my list, please report any issues to [my github repository](https://github.com/jrittenh/JustNoSinclair/issues).")
+                    submission.reply("The domain this post links to is owned or operated by [Sinclair Broadcast Group]"
+                    "(https://en.wikipedia.org/wiki/List_of_stations_owned_or_operated_by_Sinclair_Broadcast_Group).\n\n"
+                    "Rolling Stone just recently published an article titled [\"Sinclair Broadcasting's Hostile Takeover\"]"
+                    "(https://www.rollingstone.com/culture/features/sinclair-broadcast-group-hostile-takeover-trump-w519331).  "
+                    "PBS NewsHour [has reported on Sinclair's \"partisan tilt on trusted local news\"](https://youtu.be/zNhUk5v3ohE).  "
+                    "John Oliver has [featured a segment on his show](https://youtu.be/GvtNyOzGogc), Last Week Tonight, on Sinclair.  "
+                    "Most recently, Sinclair has had an instance of numerous news reports using the exact same script for a Sinclair-"
+                    "provided segment [cut together and published on YouTube](https://youtu.be/hWLjYJ4BzvI).\n\n"
+                    "I am a bot.  Please report any issues or requests to have your subreddit removed from my list to "
+                    "[my github repository](https://github.com/jrittenh/JustNoSinclair/issues).")
                     posts_replied_to.append(submission.id)
                     with open("posts_replied_to", "a") as f:
                         f.write(post.id + "\n")
